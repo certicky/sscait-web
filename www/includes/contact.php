f274cdefe7b7c93712c259669f3162fb1f7c9dba
certicky <certicky@gmail.com>
2022-12-06 16:16:29 +0100

    removed some deprecated info from Contacts page

--- www/includes/contact.php
+++ www/includes/contact.php
@@ -10,73 +10,69 @@
 </div>
 
 <h2><?php techo('Organizers:');?></h2>
-<div style="text-align: left;">
-<table id="organizers">
-	<!--
-	<tr>
-		<td><img src="./images/organizers/certicky.png" alt="" /></td>
-		<td class="name">Michal Čertický</td>
-		<td>Founder, Admin, Programming</td>
-		<td><a target="_blank" href="http://agents.fel.cvut.cz/~certicky/">homepage</a></td>
-	</tr>
-	-->
-	<tr>
-		<td colspan="2"><a href="http://gas.fel.cvut.cz/" target="_blank"><img style="width: 200px; height: 200px; border: none;" src="./images/gas-logo.png" alt="" /></a></td>
-		<td>
-			SSCAIT tournament &amp; ladder is independently organized by its founder <a href="https://certicky.github.io/" target="_blank">Michal Čertický</a> with the help of the StarCrat AI community members.
-			<br />
-			In the past, it was also maintained by <b>Games &amp; Simulations Research Group</b> at Artificial Intelligence Center, Czech Technical University in Prague.
-			<ul>
-				<!--<li>See what they do and contact them here: <a target="_blank" href="http://gas.fel.cvut.cz/">G&amp;S homepage</a></li>-->
-				<li>If you like what we do, you can support us on Patreon: <a target="_blank" href="http://patreon.com/sscait">SSCAIT Patreon</a></li>
-			</ul>
-			  
-		</td>
-		<td></td>
-	</tr>
-	<!--
-	<tr>
-		<td colspan="4" style="padding-top: 10px; padding-bottom: 20px; color: #555;">
-			There are several external volunteers who help us 
-			with the tournament or used to help in the past: 
-		</td>
-	</tr>
-	<tr>
-		<td><img src="./images/organizers/nepeta.jpg" alt="" /></td>
-		<td class="name">Paul Paradies</td>
-		<td>PR, Promo</td>
-		<td><a target="_blank" href="http://paul.paradies.nu/">homepage</a></td>
-	</tr>
-	<tr>
-		<td><img src="./images/organizers/suppa.png" alt="" /></td>
-		<td class="name">Marek Šuppa</td>
-		<td>Security, Programming (web)</td>
-		<td><a target="_blank" href="http://marek.suppa.co/">homepage</a></td>
-	</tr>
-	<tr>
-		<td><img src="./images/organizers/bjorn.jpg" alt="" /></td>
-		<td class="name">Björn Persson Mattsson</td>
-		<td>Programming (tournament module &amp; observer)</td>
-		<td><a target="_blank" href="http://plankter.se/">homepage</a></td>
-	</tr>
-	<tr>
-		<td><img src="./images/organizers/vajda.png" alt="" /></td>
-		<td class="name">Tomáš Vajda</td>
-		<td>Programming (torurnament module &amp; observer)</td>
-		<td><a target="_blank" href=""></a></td>
-	</tr>
-	<tr>
-		<td><img src="./images/organizers/rafal.jpg" alt="" /></td>
-		<td class="name">Rafał Poniatowski</td>
-		<td>Bug reporting, Programming (web)</td>
-		<td></td>
-	</tr>
-	<tr>
-		<td><img src="./images/organizers/soeren.jpg" alt="" /></td>
-		<td class="name">Sören Klett</td>
-		<td>Bug reporting</td>
-		<td><a target="_blank" href=""></a></td>
-	</tr>
-	-->
-</table>
-</div>
+<!--
+<tr>
+	<td><img src="./images/organizers/certicky.png" alt="" /></td>
+	<td class="name">Michal Čertický</td>
+	<td>Founder, Admin, Programming</td>
+	<td><a target="_blank" href="http://agents.fel.cvut.cz/~certicky/">homepage</a></td>
+</tr>
+-->
+
+<p>
+	<!-- <td colspan="2"><a href="http://gas.fel.cvut.cz/" target="_blank"><img style="width: 200px; height: 200px; border: none;" src="./images/gas-logo.png" alt="" /></a></td> -->
+	SSCAIT tournament &amp; ladder is independently organized by its founder <a href="https://certicky.github.io/" target="_blank">Michal Čertický</a> with the help of the StarCrat AI community members.
+	<br />
+	In the past, it was also maintained by <b>Games &amp; Simulations Research Group</b> at Artificial Intelligence Center, Czech Technical University in Prague.
+	<!--<li>See what they do and contact them here: <a target="_blank" href="http://gas.fel.cvut.cz/">G&amp;S homepage</a></li>-->
+</p>
+
+<p>
+	If you like what we do, you can support us on Patreon: <a target="_blank" href="http://patreon.com/sscait">SSCAIT Patreon</a>
+</p>
+
+<!--
+<tr>
+	<td colspan="4" style="padding-top: 10px; padding-bottom: 20px; color: #555;">
+		There are several external volunteers who help us
+		with the tournament or used to help in the past:
+	</td>
+</tr>
+<tr>
+	<td><img src="./images/organizers/nepeta.jpg" alt="" /></td>
+	<td class="name">Paul Paradies</td>
+	<td>PR, Promo</td>
+	<td><a target="_blank" href="http://paul.paradies.nu/">homepage</a></td>
+</tr>
+<tr>
+	<td><img src="./images/organizers/suppa.png" alt="" /></td>
+	<td class="name">Marek Šuppa</td>
+	<td>Security, Programming (web)</td>
+	<td><a target="_blank" href="http://marek.suppa.co/">homepage</a></td>
+</tr>
+<tr>
+	<td><img src="./images/organizers/bjorn.jpg" alt="" /></td>
+	<td class="name">Björn Persson Mattsson</td>
+	<td>Programming (tournament module &amp; observer)</td>
+	<td><a target="_blank" href="http://plankter.se/">homepage</a></td>
+</tr>
+<tr>
+	<td><img src="./images/organizers/vajda.png" alt="" /></td>
+	<td class="name">Tomáš Vajda</td>
+	<td>Programming (torurnament module &amp; observer)</td>
+	<td><a target="_blank" href=""></a></td>
+</tr>
+<tr>
+	<td><img src="./images/organizers/rafal.jpg" alt="" /></td>
+	<td class="name">Rafał Poniatowski</td>
+	<td>Bug reporting, Programming (web)</td>
+	<td></td>
+</tr>
+<tr>
+	<td><img src="./images/organizers/soeren.jpg" alt="" /></td>
+	<td class="name">Sören Klett</td>
+	<td>Bug reporting</td>
+	<td><a target="_blank" href=""></a></td>
+</tr>
+-->
+
