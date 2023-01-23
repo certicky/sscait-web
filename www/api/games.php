7b11432739a116ea03535cec21130cc52fa294b7
certicky <certicky@gmail.com>
2023-01-24 00:40:18 +0100

    converting old mysql_* calls to PDO: api/games.php

--- www/api/games.php
+++ www/api/games.php
@@ -31,23 +31,31 @@ require_once '../settings_server.php';
 // get MySQL-sanitized version of all the GET parameters
 $get = array();
 foreach ($_GET as $k => $v) {
-	$get[$k] = mysql_real_escape_string($v,$GLOBALS['mysqlConnection']);
+	$get[$k] = $v;
 }
 
 function getName($id) {
-	$res = mysql_query("SELECT full_name FROM fos_user WHERE id='$id'");
-	if (mysql_num_rows($res) > 0) {
-		$l = mysql_fetch_assoc($res);
-		return $l['full_name'];
-	} else return "";
+    $stmt = $GLOBALS['mysqliConnection']->prepare("SELECT full_name FROM fos_user WHERE id = :id");
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+    if($stmt->rowCount() > 0) {
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $row['full_name'];
+    } else {
+        return "";
+    }
 }
 
 function getBotId($name) {
-	$res = mysql_query("SELECT id FROM fos_user WHERE full_name='".urldecode($name)."'");
-	if (mysql_num_rows($res) > 0) {
-		$l = mysql_fetch_assoc($res);
-		return $l['id'];
-	} else return "";
+    $stmt = $GLOBALS['mysqliConnection']->prepare("SELECT id FROM fos_user WHERE full_name = :name");
+    $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+    $stmt->execute();
+    if($stmt->rowCount() > 0) {
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $row['id'];
+    } else {
+        return "";
+    }
 }
 
 function getReplayLink($gameId, $bot1Name, $bot2Name) {
@@ -77,8 +85,11 @@ if (isset($get['future']) && (strtolower($get['future']) == "false" || $get['fut
 if ((!isset($get['future'])) || ((isset($get['future']) && (strtolower($get['future']) == "true" || $get['future'] == "1")))) $futureConditions = "(result = 'unfinished' OR result = 'error')";
 
 if (isset($get['bots'])) {
-	$botStr = mysql_real_escape_string($get['bots']);
+	$search = array("\\",  "\x00", "\n",  "\r",  "'",  '"', "\x1a");
+    	$replace = array("\\\\","\\0","\\n", "\\r", "\'", '\"', "\\Z");
+    	$botStr = str_replace($search, $replace, $get['bots']);
 	$botConds = array();
+
 	foreach (explode(",",$botStr) as $bot) {
 		$botConds[] = "((bot1='".getBotId($bot)."') OR (bot2='".getBotId($bot)."'))";
 	}
@@ -94,14 +105,17 @@ if (isset($get['bots'])) {
 
 $resultStrings = array();
 if ($GLOBALS["eliminationBracketPhase"] == false) {
-    $res = mysql_query("SELECT datetime,bot1,bot2,result,map,note,game_id FROM games WHERE $futureConditions $botConditions  ORDER BY game_id DESC LIMIT ".($count*($page-1)).",".($count).";");
-    while ($n = mysql_fetch_assoc($res)) {
+    $stmt = $GLOBALS['mysqliConnection']->prepare("SELECT datetime,bot1,bot2,result,map,note,game_id FROM games WHERE $futureConditions $botConditions  ORDER BY game_id DESC LIMIT :start,:count");
+    $stmt->bindValue(':start', ($count*($page-1)), PDO::PARAM_INT);
+    $stmt->bindValue(':count', $count, PDO::PARAM_INT);
+    $stmt->execute();
+    while ($n = $stmt->fetch(PDO::FETCH_ASSOC)) {
         $n['timestamp'] = $n['datetime'];
         $n['host'] = getName($n['bot1']);
         $n['guest'] = getName($n['bot2']);
-        
+
         $n['replay'] = getReplayLink($n['game_id'],$n['host'],$n['guest']);
-        
+
         unset($n['datetime']);
         unset($n['bot1']);
         unset($n['bot2']);
